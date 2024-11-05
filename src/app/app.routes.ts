f5e8a5c2bd4e1de41932a3098d2a4fc397b957cc
Endi Brahja <endibrahja@gmail.com>
2024-11-05 15:49:18 +0100

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,29 +3,20 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () =>
-      import('./pages/splash-screen/splash-screen.page').then(
-        (m) => m.SplashScreenPage
-      ),
-  },
-  {
-    path: 'home',
-    loadComponent: () =>
-      import('./pages/home/home.page').then((m) => m.HomePage),
-  },
-  {
-    path: 'about',
-    loadComponent: () =>
-      import('./pages/about/about.page').then((m) => m.AboutPage),
-  },
-  {
-    path: '',
-    redirectTo: 'splash',
+    redirectTo: 'login',
     pathMatch: 'full',
   },
   {
     path: '**',
     redirectTo: 'home',
     pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
   },
 ];
---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonSpinner } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonicModule, IonSpinner]
+})
+
+export class LoginPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
